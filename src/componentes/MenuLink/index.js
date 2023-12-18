456dcd56a8b6356359cdd20fd943ee712fdd0725
Quintero <guiquintero02@gmail.com>
2023-12-18 08:11:00 -0300

    upload the start project

---
+++ src/componentes/MenuLink/index.js
@@ -0,0 +1,18 @@
+import { Link, NavLink, useLocation } from 'react-router-dom';
+import styles from './MenuLink.module.css';
+
+export default function MenuLink({ children, to}) {
+    return (
+        <NavLink
+        className={({ isActive }) => `
+            ${styles.link}
+            ${isActive ? styles.linkDestacado : ""}
+        `}
+        to={to}
+        >
+            {children}
+        </NavLink>
+
+
+    )
+}
